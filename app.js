d227c6bd5f4196734676c6d48b0bdc9483c2c41b
Hoang Duong <hoangduongphantri@gmail.com>
2023-03-01 17:01:16 +0700

    Exercise 11.11 done

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 const express = require ('express');
 const app = express ();
 
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000;
+
 app.get ('/health', (req, res) => {
   res.send ('hello');
 });
-
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
 
 app.use (express.static ('dist'));
 
